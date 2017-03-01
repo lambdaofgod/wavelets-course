721f99b5287a8cfce85dec0a59b4917ac2c67c2b
lambdaofgod <bartczukkuba@gmail.com>
2017-03-01 22:57:46 +0100

    refactored plotting

--- helpers.py
+++ helpers.py
@@ -3,15 +3,26 @@ import pywt
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 
-def plot_wavelet_approximations(apxs):
+gray_plotter = lambda img: plt.imshow(img, cmap='gray', vmax = 255, vmin=0)
+
+def plot_linear_layout(imgs, plotter = gray_plotter):
+  N = len(imgs)
+  gs = GridSpec(1, N)
+  gs.update(wspace=0, hspace=0)
+  plot_with_gs(imgs, gs, plotter, N)
+
+def plot_rectangular_layout(imgs, plotter = gray_plotter):
   N = len(apxs)
-  n = int(sqrt(len(apxs)))
+  n = int(sqrt(len(imgs)))
   gs = GridSpec(n, n)
   gs.update(wspace=0, hspace=0)
+  plot_with_gs(imgs, gs, plotter, N)
 
+def plot_with_gs(imgs, gridspec, plotter, N):
   for i in range(N):
-    img = apxs[i]
-    plt.subplot(gs[i])
-    plt.imshow(img, cmap='gray')
+    img = imgs[i]
+    plt.subplot(gridspec[i])
+    plotter(img)
     plt.axis('off')
   plt.show()
+
